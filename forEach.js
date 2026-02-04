c1166eadc60eaac6a055eb8c5a135e07ed0172c4
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-02-04 23:16:36 +0600

    added forEach

---
+++ forEach.js
@@ -0,0 +1,18 @@
+const numbers =[1,2,3,4];
+// numbers.forEach(x=>console.log(x))
+//output:
+// 1
+// 2
+// 3
+// 4
+numbers.forEach(num =>{
+    const double = num*2;
+    console.log(double)
+})
+/**
+ * output :
+ * 2
+ * 4
+ * 6
+ * 8
+*/
